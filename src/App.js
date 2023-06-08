f45dc18579fd2a5b87fc7d5baba62298b2760cfa
Nilambar Sharma <nilambar@outlook.com>
2023-06-08 21:44:59 +0545

    Add files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{'marginTop':'50px','color':'#333333'}}>Hello React!!!</h1>
+    </div>
+  );
+}
+
+export default App;
