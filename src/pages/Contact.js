77d1e61cd1f33b100a7ce090a5db26700fd9484a
Nilambar Sharma <nilambar@outlook.com>
2023-06-12 21:05:15 +0545

    Add contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,6 +2,7 @@ import SiteHeader from '../components/SiteHeader/SiteHeader';
 import SiteFooter from '../components/SiteFooter/SiteFooter';
 import Container from '../components/Container/Container';
 import PageHeader from '../components/PageHeader/PageHeader';
+import ContactForm from '../components/ContactForm/ContactForm';
 
 const Contact = () => {
 	return (
@@ -9,7 +10,7 @@ const Contact = () => {
 			<SiteHeader />
 			<PageHeader title="Contact" />
 			<Container>
-				<p>Contact form will be here.</p>
+				<ContactForm />
 			</Container>
 			<SiteFooter />
 		</div>
