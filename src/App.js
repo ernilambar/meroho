ecece71fc9e320436e836b16a3974a5f0d55dd83
Nilambar Sharma <nilambar@outlook.com>
2023-06-12 13:20:40 +0545

    Add bio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Header from './components/Header'
+import Bio from './components/Bio';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Bio />
     </div>
   );
 }
---
+++ src/components/Bio.js
@@ -0,0 +1,11 @@
+const Bio = () => {
+  return (
+    <div className='App-bio'>
+      <div className='App-bio-inner'>
+        <p>Hello! I am Nilambar Sharma from the beautiful country of Nepal. I am a Full Stack Web Developer, WordPress enthusiast, Freelancer and blogger. In free time, I like to watch movies and listen music.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Bio;
