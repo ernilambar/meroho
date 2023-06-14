47df416eb2a526ac7f88ec39b943345db621bd98
Nilambar Sharma <nilambar@outlook.com>
2023-06-14 17:08:58 +0545

    feat: formik

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { useFormik } from 'formik';
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import './ContactForm.scss';
 
@@ -7,130 +7,108 @@ const FORM_ENDPOINT =
 	'https://public.herotofu.com/v1/03baeb70-08f7-11ee-8267-d3eb100789b4';
 
 const ContactForm = () => {
-	const [status, setStatus] = useState('Submit');
-
-	const formik = useFormik({
-		initialValues: {
-			name: '',
-			email: '',
-			message: '',
-		},
-
-		validationSchema: Yup.object({
-			name: Yup.string()
-				.max(20, 'Name must be 20 characters or less.')
-				.required('Name is required'),
-			email: Yup.string()
-				.email('Invalid email address')
-				.required('Email is required'),
-			message: Yup.string().required('Message is required'),
-		}),
-
-		onSubmit: (values) => {
-			postContactForm(values);
-		},
-	});
-
-	const postContactForm = (data) => {
-		// console.log(data);
-		return;
-
-		fetch(FORM_ENDPOINT, {
-			method: 'POST',
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(data),
-		})
-			.then((response) => {
-				if (response.status === 422) {
-					// challenge.
-				}
-
-				if (response.status !== 200) {
-					throw new Error(response.statusText);
-				}
-
-				return response.json();
-			})
-			.then(() => setStatus('Submitted successfully!'))
-			.catch((err) => setStatus(err.toString()));
-	};
-
 	return (
-		<form
-			onSubmit={formik.handleSubmit}
-			className="contact-form"
-			acceptCharset="UTF-8"
+		<Formik
+			initialValues={{ name: '', email: '', message: '' }}
+			validationSchema={() => {
+				return Yup.object({
+					name: Yup.string()
+						.max(20, 'Name must be 20 characters or less.')
+						.required('Name is required'),
+					email: Yup.string()
+						.email('Invalid email address')
+						.required('Email is required'),
+					message: Yup.string().required('Message is required'),
+				});
+			}}
+			onSubmit={(values, { setSubmitting, resetForm }) => {
+				fetch(FORM_ENDPOINT, {
+					method: 'POST',
+					headers: {
+						Accept: 'application/json',
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify(values),
+				})
+					.then((response) => {
+						if (response.status === 422) {
+							// challenge.
+						}
+
+						if (response.status !== 200) {
+							throw new Error(response.statusText);
+						}
+
+						return response.json();
+					})
+					.then(() => {
+						// console.log('Submitted successfully');
+						resetForm({ values: '' });
+						setSubmitting(false);
+					})
+					.catch(() => {});
+			}}
 		>
-			<div>
-				<label htmlFor="name">Name:</label>
-				<input
-					type="text"
-					name="name"
-					id="name"
-					placeholder="Enter full name"
-					onChange={formik.handleChange}
-					value={formik.values.name}
-					onBlur={formik.handleBlur}
-				/>
-				{formik.touched.name && formik.errors.name && (
-					<div className="contact-form-input-error">
-						{formik.errors.name}
+			{({ isSubmitting }) => (
+				<Form className="contact-form">
+					<div>
+						<label htmlFor="name">Name:</label>
+						<Field type="text" name="name" placeholder="Name" />
+						<ErrorMessage name="name">
+							{(msg) => (
+								<div className="contact-form-input-error">
+									{msg}
+								</div>
+							)}
+						</ErrorMessage>
 					</div>
-				)}
-			</div>
-			<div>
-				<label htmlFor="email">Email:</label>
-				<input
-					type="email"
-					name="email"
-					id="email"
-					placeholder="Enter email"
-					onChange={formik.handleChange}
-					value={formik.values.email}
-					onBlur={formik.handleBlur}
-				/>
-				{formik.touched.email && formik.errors.email && (
-					<div className="contact-form-input-error">
-						{formik.errors.email}
+					<div>
+						<label htmlFor="email">Email:</label>
+						<Field type="email" name="email" placeholder="Email" />
+						<ErrorMessage name="email">
+							{(msg) => (
+								<div className="contact-form-input-error">
+									{msg}
+								</div>
+							)}
+						</ErrorMessage>
 					</div>
-				)}
-			</div>
-			<div>
-				<label htmlFor="message">Message:</label>
-				<textarea
-					name="message"
-					id="message"
-					placeholder="Enter your message"
-					onChange={formik.handleChange}
-					value={formik.values.message}
-					onBlur={formik.handleBlur}
-				/>
-				{formik.touched.message && formik.errors.message && (
-					<div className="contact-form-input-error">
-						{formik.errors.message}
+					<div>
+						<label htmlFor="message">Message:</label>
+						<Field
+							as="textarea"
+							name="message"
+							placeholder="Message"
+						/>
+						<ErrorMessage name="message">
+							{(msg) => (
+								<div className="contact-form-input-error">
+									{msg}
+								</div>
+							)}
+						</ErrorMessage>
 					</div>
-				)}
-			</div>
-			<button type="submit">{status}</button>
-			<div
-				style={{
-					overflow: 'hidden',
-					position: 'absolute',
-					whiteSpace: 'nowrap',
-					textIndent: '-99999px',
-				}}
-			>
-				<input
-					type="text"
-					name="_gotcha"
-					tabIndex="-1"
-					autoComplete="off"
-				/>
-			</div>
-		</form>
+					<button type="submit" disabled={isSubmitting}>
+						Submit
+					</button>
+					<div
+						style={{
+							overflow: 'hidden',
+							position: 'absolute',
+							whiteSpace: 'nowrap',
+							textIndent: '-99999px',
+						}}
+					>
+						<input
+							type="text"
+							name="_gotcha"
+							tabIndex="-1"
+							autoComplete="off"
+						/>
+					</div>
+				</Form>
+			)}
+		</Formik>
 	);
 };
 
