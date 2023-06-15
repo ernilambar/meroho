63e740fb194596631bc18115709129d2931a9711
Nilambar Sharma <nilambar@outlook.com>
2023-06-15 17:04:16 +0545

    feat: typer

--- src/components/Author/Author.js
+++ src/components/Author/Author.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
+import Typed from 'typed.js';
 import Social from '../Social/Social';
 import './Author.scss';
 import avatar from './../../images/avatar.jpeg';
 
 const Author = () => {
+	const typerElement = React.useRef(null);
+
+	useEffect(() => {
+		const typed = new Typed(typerElement.current, {
+			strings: ['Software Developer', 'Freelancer', 'Blogger'],
+			typeSpeed: 80,
+			showCursor: false,
+			loop: true,
+		});
+
+		return () => {
+			typed.destroy();
+		};
+	}, []);
+
 	return (
 		<div className="author">
 			<div className="author-content">
@@ -14,7 +30,7 @@ const Author = () => {
 				<div>
 					<div className="author-details">
 						<h2 className="author-name">Nilambar Sharma</h2>
-						<h4 className="author-position">Software Developer</h4>
+						<span className="author-position" ref={typerElement} />
 						<Social />
 					</div>
 				</div>
--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Container from '../components/Container/Container';
+import useDocumentTitle from '../components/DocumentTitle/useDocumentTitle';
 
 const NoPage = () => {
+	useDocumentTitle('Page not found - Nilambar Sharma');
 	return (
 		<div className="no-page">
-			<Container>
-				<div style={{ width: '400px', color: '#ffffff' }}>
-					<p>The page you are looking for does not exist.</p>
-				</div>
-			</Container>
+			<div style={{ color: '#ffffff' }}>
+				<h1>404 Error!</h1>
+				<p>The page you are looking for does not exist.</p>
+			</div>
 		</div>
 	);
 };
