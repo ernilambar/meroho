3a2794b317b87dfc5be30aa19f8e25edf20718df
Nilambar Sharma <nilambar@outlook.com>
2023-06-22 21:09:20 +0545

    fix: resume

---
+++ src/components/Resume/Resume.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Content from '../../containers/Content/Content';
+import Box from '../../containers/Box/Box';
+
+const Resume = () => {
+	return (
+		<Content>
+			<Box heading="My" heading2="Resume">
+				<div>
+					<p>Resume will be here.</p>
+				</div>
+			</Box>
+		</Content>
+	);
+};
+
+export default Resume;
